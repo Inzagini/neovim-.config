cb83f474a7f40de043f8c6fc26e7620822ba4224
Inzagini <toggo1212@gmail.com>
2026-02-15 19:53:50 +0100

    add C++ for lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -178,6 +178,17 @@ return {
 					},
 				},
 			},
+			clangd = {
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--header-insertion=iwyu",
+					"--completion-style=detailed",
+					"--function-arg-placeholders",
+					"--fallback-style=llvm",
+				},
+			},
 		}
 
 		-- Ensure the servers and tools above are installed
